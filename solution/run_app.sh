04564a50373ecb0a0a759389596643eb15d790b8
sukanto <mondalda@gmail.com>
2019-06-25 12:13:54 -0700

    Question 0: Initial

--- solution/run_app.sh
+++ solution/run_app.sh
@@ -4,7 +4,9 @@
 
 #create the docker image
 echo "Creating image for the app..."
-BUILD_RESULT=$(docker build --rm -t ufoapp ufo-microservice/)
+#copy the data in docker build contex
+cp ../ufo-sightings.csv ufo-sightings-microservice/util/ufo-sightings.csv
+BUILD_RESULT=$(docker build --rm -t ufoapp ufo-sightings-microservice/) 
 
 if echo $BUILD_RESULT | grep -q "Successfully tagged ufoapp:latest"; then
     echo "Successfully tagged ufoapp:latest"
@@ -18,6 +20,20 @@ echo "Description: Using command 'docker run -itd --rm --name ufo -p 8888:80 ufo
 id=$(docker run -itd --rm --name ufo -p 8888:80 ufoapp)
 echo ${id}
 
+sleep 5
+
+# set the root password for mysql
+echo "Setting root password for mysql ..."
+sudo docker exec -it ufo /root/set_mysql_root_password.sh root123
+
+sleep 5
+
+#make database for ufo-sighting-app
+echo "Making database for ufo-sightings..."
+sudo docker exec -it ufo /root/create_database_for_ufo_sightings.sh
+
+sleep 5
+
 # run the project installation script 
 echo "Installing the project..."
-sudo docker exec -it ufo /root/project_install.sh ufo-sightings
+sudo docker exec -it ufo /root/project_install.sh ufo_sightings_app
--- solution/ufo-sightings-microservice/util/project_install.sh
+++ solution/ufo-sightings-microservice/util/project_install.sh
@@ -10,69 +10,22 @@ PROJECT_DIR=$(pwd)
 
 cd ${PROJECT_DIR}
 
+mkdir -p ${PROJECT_DIR}/${PROJECT_NAME}
 
-#The directory for the project
-mkdir -p ${PROJECT_NAME}/src/public
-
-# PHP composer install
-wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
-mv composer.phar ${PROJECT_DIR}/${PROJECT_NAME}/src/composer.phar
-
-#run composer to install slim framework
-(cd ${PROJECT_NAME}/src && php ${PROJECT_DIR}/${PROJECT_NAME}/src/composer.phar require slim/slim)
-
-echo "$(tput setaf 2)Creating project skelton ...$(tput sgr 0)"
-
-touch ${PROJECT_NAME}/src/public/index.php
-
-
-#index file templet content 
-cat <<EOF > ${PROJECT_NAME}/src/public/index.php
-<?php
-use \\Psr\\Http\\Message\\ServerRequestInterface as Request;
-use \\Psr\\Http\\Message\\ResponseInterface as Response;
-
-require '../vendor/autoload.php';
-
-\$app = new \\Slim\\App;
-\$app->get('/hello/{name}', function (Request \$request, Response \$response, array \$args) {
-    \$name = \$args['name'];
-    \$response->getBody()->write("Hello, \$name");
-
-    return \$response;
-});
-\$app->run();
-EOF
-
-# creating .htaccs file
-touch ${PROJECT_NAME}/src/public/.htaccess
-
-cat <<EOF > ${PROJECT_NAME}/src/public/.htaccess
-RewriteEngine on
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteRule . index.php [L]
-EOF
-
-
-echo "$(tput setaf 2)Configuring apache...$(tput sgr 0)"
-
-# configure apache to run api
+#copy project source file
+cp /root/ufo-sightings/src ${PROJECT_DIR}/${PROJECT_NAME}/src -r
 
 cat <<EOF >> /etc/httpd/conf/httpd.conf
 <Directory "${PROJECT_DIR}/${PROJECT_NAME}/src/public">
-    Options Indexes MultiViews FollowSymLinks
-    AllowOverride All
-    Order allow,deny
-    Allow from all
+   Options Indexes MultiViews FollowSymLinks
+   AllowOverride All
+   Order allow,deny
+   Allow from all
 </Directory>
 
 <VirtualHost *:80>
-    DocumentRoot ${PROJECT_DIR}/${PROJECT_NAME}/src/public
+      DocumentRoot ${PROJECT_DIR}/${PROJECT_NAME}/src/public
 </VirtualHost>
 EOF
-
 # restart apache
 service httpd restart
-
-echo "$(tput setaf 2)Everthing looks OK!!! The Templet successfully installed.$(tput sgr 0)"
